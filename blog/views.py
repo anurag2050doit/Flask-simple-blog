f68b8bd982885a6c00ae1c4b946d3746041e37a0
Anurag <anurag2050@gmail.com>
2017-03-26 22:22:50 +0530

    Simple Blog created

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,48 @@
-from flask_blog import app
+from flask_blog import app , db
+from flask import render_template, redirect, flash , url_for
+from blog.models import Blog
+from user.models import User
+from blog.form import SetupForm
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello World!"
+
+@app.route('/admin')
+def admin():
+    blogs = Blog.query.count()
+    if blogs == 0:
+        return redirect(url_for('setup'))
+    return render_template('blog/admin.html')
+
+@app.route('/setup', methods=('GET','POST'))
+def setup():
+    form = SetupForm()
+    error = None
+    if form.validate_on_submit():
+        user = User(
+            form.fullname.data,
+            form.email.data,
+            form.username.data,
+            form.password.data,
+            True
+        )
+        db.session.add(user)
+        db.session.flush()
+        if user.id:
+            blog = Blog(form.name.data, user.id)
+            db.session.add(blog)
+            db.session.flush()
+        else:
+            db.session.rollback()
+            error = "Error creating user"
+        if user.id and blog.id:
+            db.session.commit()
+            flash('Blog created')
+            return redirect(url_for('admin'))
+        else:
+            db.session.rollback()
+            error = "Error creating blog"
+    return   render_template('blog/setup.html', form=form, error=error)
