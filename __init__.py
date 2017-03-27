605d0ea713cad9649f7ba780c50af13ab2a09ce1
Anurag <anurag2050@gmail.com>
2017-03-28 00:48:44 +0530

    Added Markdown

--- __init__.py
+++ __init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
+from flask.ext.markdown import Markdown
 app = Flask(__name__)
 app.config.from_object('settings')
 db = SQLAlchemy(app)
 
+# migrations
 Migrate = Migrate(app, db)
+
+# Markdown
+md = Markdown(app,extensions=['fenced_code','tables'])
 
 from blog import views
 from user import views
