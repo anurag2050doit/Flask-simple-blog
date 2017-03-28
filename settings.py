631d45366021d51539d308f3dbce263f8b81353f
Anurag <anurag2050@gmail.com>
2017-03-28 12:33:08 +0530

    Image Render Added

--- settings.py
+++ settings.py
@@ -2,5 +2,5 @@ SECRET_KEY = "SECRET_KEY"
 DEBUG = True
 SQLALCHEMY_DATABASE_URI = 'sqlite:///blog.sqlite3'
 POSTS_PER_PAGE = 5
-UPLOADED_IMAGES_DEST = 'public/static/images'
-UPLOADED_IMAGES_URL = 'public/static/images'
+UPLOADED_IMAGES_DEST = 'static/images'
+UPLOADED_IMAGES_URL = '/static/images/'
